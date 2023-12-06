7bc31b2b207a8adeb16c5e28a1e36ebd08e5af3e
Felipe Martins do Nascimento <fefisfefe@gmail.com>
2023-12-06 20:58:33 -0300

    Update InstallUbuntu.sh

--- Agent/InstallUbuntu.sh
+++ Agent/InstallUbuntu.sh
@@ -1,12 +1,7 @@
 # Configuração do Zabbix Agent2
 # Sistema Operacional: Ubuntu 22.04 LTS
 
-# Obter o Hostname
 clear
-echo "Digite o hostname (letras maiusculas)"
-read CLIENTE
-ZabbixHost="Hostname=TECNO-"
-ZabbixHost+=$CLIENTE
 
 # Instalação das dependencias
 wget https://repo.zabbix.com/zabbix/6.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.4-1+ubuntu22.04_all.deb
@@ -17,7 +12,9 @@ apt update
 apt install zabbix-agent2 -y
 
 # Configuração do Agent2
-rm /etc/zabbix/zabbix_agent2.conf
+ZabbixHost="Hostname=TECNO-"
+ZabbixHost+=$HOSTNAME
+rm /etc/zabbix/zabbix_agent2.conf -vf
 {
     echo '# Configurações de Log'
     echo 'PidFile=/var/run/zabbix/zabbix_agent2.pid'
@@ -27,7 +24,7 @@ rm /etc/zabbix/zabbix_agent2.conf
     echo '# Configuração do Zabbix Server/Proxy'
     echo 'Server=20.0.0.10,10.10.1.115'
     echo 'ServerActive=20.0.0.10,10.10.1.115'
-    echo  $CLIENTE
+    echo $ZabbixHost   
     echo ''
     echo '# Configurações Adicionais'
     echo 'Include=/etc/zabbix/zabbix_agent2.d/*.conf'
@@ -43,3 +40,6 @@ systemctl restart zabbix-agent2.service
 
 # Adicionar a regra ao firewall UFW
 sudo ufw allow 10050/tcp
+
+# Remove o lixo da instalação
+rm zabbix-release_*
