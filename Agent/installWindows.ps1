6df55796e652306d70f29c3b169161556465cf0e
Felipe Martins do Nascimento <38166696+ShadowFFox@users.noreply.github.com>
2023-12-12 17:23:36 -0300

    Create installWindows.ps1

---
+++ Agent/installWindows.ps1
@@ -0,0 +1,35 @@
+# Elevação automática do script para Administrador
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+# Variáveis definidas
+param ($zbxHost)
+$zbxFolder   =   "C:\Program Files\Zabbix\"
+$zbxConf    =   @"
+LogFile=$zbxFolder\zabbix_agent2.log
+LogFileSize=5
+DebugLevel=3
+Server=$zbxHost
+ServerActive=$zbxHost
+Hostname=$env:COMPUTERNAME
+ControlSocket=\\.\pipe\agent.sock
+Include=.\zabbix_agent2.d\plugins.d\*.conf
+"@
+
+# Remove arquivos antigos
+
+# Download do Zabbix Agent2 6.4
+Invoke-WebRequest -OutFile $env:Temp\zabbix.zip https://cdn.zabbix.com/zabbix/binaries/stable/6.4/6.4.9/zabbix_agent2-6.4.9-windows-amd64-static.zip
+New-Item -Name "Zabbix" -Path "C:\Program Files\" -ItemType Directory
+Expand-Archive -Path $env:Temp/zabbix.zip -DestinationPath $zbxFolder
+Remove-Item -Path $env:Temp\zabbix.zip
+
+# Edição do arquivo de configuração padrão
+Remove-Item -Path $zbxFolder\conf\zabbix_agent2.conf # Remoção do arquivo antigo
+New-Item -Name "zabbix_agent2.conf" -Path $zbxFolder\conf -ItemType File -Value $zbxConf # Inclusão do arquivo novo
+
+# Realiza a instalação do agent
+Set-Location $zbxFolder\bin
+.\zabbix_agent2.exe -c $zbxFolder\conf\zabbix_agent2.conf -i
+Stop-Service 'Zabbix Agent 2'
+Start-Service 'Zabbix Agent 2'
+Pause
